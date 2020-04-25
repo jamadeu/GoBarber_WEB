99de3eea5aeb9f8ad6acf30cb6de89e83bd88b6c
jamadeu <jean.amadeu2@gmail.com>
2020-04-24 21:22:07 -0300

    Feat: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+
+import Routes from './Routes';
 
 import AppProvider from './context';
 
 const App: React.FC = () => (
-  <>
+  <BrowserRouter>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
